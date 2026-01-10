ab4fcdc4a84f6f8e95f81296fae8d2a4dcf101f5
Darshangowda ML <darshangowda.ml@in.abb.com>
2026-01-11 00:40:33 +0530

    Revert "numpy excercises"
    This reverts commit bb09905b30b43473697856197ca50f8fb71ef567.

--- Numpy/numpyExcercise/handsons.py
+++
@@ -1,9 +0,0 @@
-import numpy as np 
-
-a1 = np.array([1,2,3]) #1D array
-a2 = np.array([[1,2,3],[4,5,6]]) #2D array 
-a3 = np.array([[[1,2,3],[4,5,6],[7,8,9]]]) #3D array
-
-print(a1,'\n')
-print(a2,'\n')
-print(a3)
